6759dbef3def33f2ce9ee6e1bae80e9b4e11324f
Jayant Shrivastava <jshrivastava03@gmail.com>
2019-05-18 15:19:20 -0400

    Added Update Method to Backend

--- backend/db/client.py
+++ backend/db/client.py
@@ -1,5 +1,16 @@
 from .models import RecycledMaterial
-
+from . import db
 
 def get_all_recycling_data():
+    return RecycledMaterial.query.all()
+
+def update_record(teacher, category, amount):
+
+    new_data = {}
+    new_data[category] = amount
+    a = RecycledMaterial.query.filter_by(teacher=teacher)
+
+    record = RecycledMaterial.query.filter_by(teacher=teacher).update(new_data)
+    db.session.commit()
+
     return RecycledMaterial.query.all()
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,7 @@
+import json
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 
 from .db import client as db_client
 
@@ -41,3 +43,49 @@ def get_scores_by_class():
         else: classes[teacher] = score
 
     return jsonify(classes)
+
+# Params: New Record
+# Returns: Score Record Object
+@blueprint.route('/new_entry', methods=['POST'])
+def add_or_update_new_entry():
+
+    # recycling records
+    data = [row.to_dict() for row in db_client.get_all_recycling_data()]
+
+    # incoming post data
+    post_data = json.loads(request.args['data'])
+
+    # list of recuclable fields
+    categories = [
+        k for k in data[0]
+    ]
+    categories.remove('id')
+    categories.remove('teacher')
+
+    # list of teachers
+    teachers = [record['teacher'] for record in data]
+
+    try:
+        # Get params and throw errors for invalid ones
+        teacher = str(post_data['teacher'])
+        category = str(post_data['category'])
+        amount = int(post_data['amount'])
+        if category not in categories:
+            raise ValueError 
+ 
+        if teacher in teachers:
+            new_amount = 0
+            for record in data:
+                if record['teacher'] == teacher:
+                    new_amount = record[category] + amount
+            data_to_return = [row.to_dict() for row in db_client.update_record(teacher, category, new_amount)]
+            return jsonify(data_to_return)
+
+        else:
+            db_client.insert_record(teacher, amount, category)
+            
+
+    except ValueError:
+        print('Invalid Amount or Category')
+    except KeyError:
+        print('Invalid request params')
