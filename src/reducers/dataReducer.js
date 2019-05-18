4fd2a3627dcc802c3e0ddd406296b96061592deb
Peiyao Chen <peiyao.chen@hotmail.com>
2019-05-18 13:36:02 -0400

    Add the action and reducers for fetching scores

--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -1,4 +1,4 @@
-import { LOAD_DATA, CLEAR_DATA } from '../types';
+import { LOAD_DATA, CLEAR_DATA, LOAD_SCORES } from '../types';
 
 const defaultState = {
   data: []
@@ -12,14 +12,12 @@ export default (state = defaultState, action) => {
   switch (action.type) {
     // directly sets the global data to the payload as specified in the action
     case LOAD_DATA:
-      return {
-        data: action.payload
-      }
+      return Object.assign({}, state, { data: action.payload });
     // directly sets the global data to an empty array
     case CLEAR_DATA: 
-      return {
-        data: []
-      }
+      return Object.assign({}, state, { data: []});
+    case LOAD_SCORES:
+      return Object.assign({}, state, { scores: [] });
     default:
       return state
   }
