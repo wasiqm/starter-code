9439b9982c4b32727908da031a2b745b8809ef05
Jayant Shrivastava <jshrivastava03@gmail.com>
2019-05-18 13:21:52 -0400

    Backend; New Endpoint; scores"

--- backend/routes.py
+++ backend/routes.py
@@ -10,3 +10,34 @@ blueprint = Blueprint('api', __name__)
 def get_recycling_data():
     data = [row.to_dict() for row in db_client.get_all_recycling_data()]
     return jsonify(data)
+
+# Params: None
+# Returns: Score Record Object
+@blueprint.route('/scores')
+def get_scores_by_class():
+
+    bonus_items = [
+        'batteries', 
+        'computer_parts'
+    ]
+    meta_fields = [
+        'teacher', 
+        'id'
+    ]
+    data = [row.to_dict() for row in db_client.get_all_recycling_data()]
+    classes = {}
+
+    for record in data:
+        score = 0
+        teacher = record['teacher']
+        for field in record: 
+            if field not in meta_fields: 
+                if field in bonus_items:
+                    score += 2*record[field]
+                else: 
+                    score += record[field]
+
+        if teacher in classes: classes[teacher] += score
+        else: classes[teacher] = score
+
+    return jsonify(classes)
